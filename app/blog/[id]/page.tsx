454e92c0b6975771598685e8bbc65f4669cc43a6
Daniil Chukarin <chukarindeniil@gmail.com>
2024-09-29 16:40:36 +0300

    fix error

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next";
-import { title } from "process";
 
 async function getData(id: string){
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,
